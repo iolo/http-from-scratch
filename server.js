12b3fc12c630c21e23eeda8488f2ee65cdd540cf
Dongsu Jang <77693+iolo@users.noreply.github.com>
2022-05-31 16:31:53 +0900

    add response header

--- server.js
+++ server.js
@@ -13,6 +13,8 @@ server.on('connection', (socket) => {
     console.log('server:socket:data', data);
 
     socket.write('HTTP/1.0 200 OK\r\n');
+    socket.write('Server: server/0.0.1\r\n');
+    socket.write('Content-Type: text/plain\r\n');
     socket.write('\r\n');
     socket.write('Hello,World!\r\n');
     socket.end();
