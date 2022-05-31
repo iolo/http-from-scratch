82e4d0b92ec72d741d7e79a3f5cf2c1f5f84a612
Dongsu Jang <77693+iolo@users.noreply.github.com>
2022-05-31 16:38:07 +0900

    parse request line and headers

--- server.js
+++ server.js
@@ -12,11 +12,28 @@ server.on('connection', (socket) => {
   socket.on('data', (data) => {
     console.log('server:socket:data', data);
 
+    const [headPart, bodyPart] = data.split('\r\n\r\n', 2);
+
+    const [requestLine, ...headerLines] = headPart.split('\r\n');
+    const [method, requestUri, httpVersion] = requestLine.split(' ', 3);
+    const headers = headerLines.reduce((result, headerLine) => {
+      const [name, value] = headerLine.split(':', 2);
+      result[name.trim().toLowerCase()] = value.trim();
+      return result;
+    }, {});
+
     socket.write('HTTP/1.0 200 OK\r\n');
     socket.write('Server: server/0.0.1\r\n');
     socket.write('Content-Type: text/plain\r\n');
     socket.write('\r\n');
     socket.write('Hello,World!\r\n');
+    socket.write(`method: ${method}\r\n`);
+    socket.write(`requestUri: ${requestUri}\r\n`);
+    socket.write(`httpVersion: ${httpVersion}\r\n`);
+    socket.write('headers:\r\n');
+    for (const [name, value] of Object.entries(headers)) {
+      socket.write(`* ${name}:${value}\r\n`);
+    }
     socket.end();
   });
 
